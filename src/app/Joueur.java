e17fdd9bc63c2b22532e5805ab94e2728ed2b971
corentinlesage <corentin.lesage@etud.u-picardie.fr>
2019-06-07 16:26:45 +0200

    implementations des méthodes stats de joueur

--- src/app/Joueur.java
+++ src/app/Joueur.java
@@ -1,6 +1,6 @@
 package app;
 
-public class Joueur {
+public class Joueur implements Stats{
 
 	private int id;
 	private int [][] presenceTerrain;
@@ -10,7 +10,33 @@ public class Joueur {
 	private float direction; 
 	private float energy; 
 	private float speed; 
-	private float total_distance; 
+	private float total_distance;
+	private int nbPosition;
+	
+	
+	
+	@Override
+	public double[][] calculerStats() {
+		
+		double[][] stats = null;
+		for (int i=0; i<presenceTerrain.length;i++)
+		{
+			for(int j = 0 ; j<presenceTerrain.length;j++)
+			{
+				
+				stats[i][j] = (presenceTerrain[i][j] / nbPosition)*100 ; 
+				
+			}
+		}
+
+		return stats;
+	}
+	@Override
+	public void ajouterPresence() {
+		presenceTerrain[(int)x_pos][(int)y_pos] =  presenceTerrain[(int)x_pos][(int)y_pos]++;
+		this.nbPosition ++;
+		
+	} 
 		
 		
 	
